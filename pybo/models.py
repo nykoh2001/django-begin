a1fc0a453bb8817ed93f14ba2706fcb5325131f1
nykoh2001 <nykoh2001@gmail.com>
2023-06-21 16:05:26 +0900

    feat: pybo req/res form

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Question:
+  subject = models.CharField(max_length=200)
+  content = models.TextField()
+  create_date = models.DateTimeField()
+  
+class Answer:
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  content = models.TextField()
+  create_date = models.DateTimeField()
+  
+
+  
