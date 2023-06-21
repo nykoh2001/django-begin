6b68b54efb4203a2aae914e84efb688bfa8eb1d0
nykoh2001 <nykoh2001@gmail.com>
2023-06-21 17:26:14 +0900

    feat: question detail page

--- pybo/views.py
+++ pybo/views.py
@@ -8,3 +8,8 @@ def index(req):
   question_list = Question.objects.order_by('-create_date')
   context = {'question_list': question_list}
   return render(req, 'pybo/question_list.html', context)
+
+def detail(req, question_id):
+  question = Question.objects.get(id=question_id)
+  context = {'question': question}
+  return render(req, 'pybo/question_detail.html', context)
