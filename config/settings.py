f5bc17805f750c03ef5f65d6325713328c7b6ead
nykoh2001 <nykoh2001@gmail.com>
2023-06-23 15:19:17 +0900

    ci: common app

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pybo.apps.PyboConfig'
+    'pybo.apps.PyboConfig',
+    'django.contrib.auth',   # login
+    'common.apps.CommonConfig' # common app config
 ]
 
 MIDDLEWARE = [
