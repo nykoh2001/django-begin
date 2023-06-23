41a315a0fb3dd2a7f461b5805d824b4a2a92b7e5
nykoh2001 <nykoh2001@gmail.com>
2023-06-23 19:45:50 +0900

    feat: sign up

--- common/views.py
+++ common/views.py
@@ -1,5 +1,19 @@
-# from django.shortcuts import render
-# # Create your views here.
+from django.contrib.auth import login, authenticate
+from .forms import UserForm
+from django.shortcuts import render, redirect
 
-# def login(req):
-  
+def signup(req):
+  if req.method == "POST":
+    form = UserForm(req.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      raw_password = form.cleaned_data.get('password1')
+      user = authenticate(username=username, password=raw_password)
+      login(req, user)
+      redirect("index")
+  else:
+    form = UserForm()
+  return render(req, 'common/signup.html', {'form': form})
+
+      
