cd986b95b6b359307f5c3b636dbe345014543ecd
nykoh2001 <nykoh2001@gmail.com>
2023-06-25 16:25:14 +0900

    feat: 질문/답변 수정/삭제

--- pybo/models.py
+++ pybo/models.py
@@ -7,6 +7,7 @@ class Question(models.Model): # inherit Model class
   subject = models.CharField(max_length=200)
   content = models.TextField()
   create_date = models.DateTimeField()
+  modify_date = models.DateTimeField(null=True, blank=True)
   
   def __str__(self) -> str:
     return self.subject
@@ -16,6 +17,7 @@ class Answer(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   content = models.TextField()
   create_date = models.DateTimeField()
+  modify_date = models.DateTimeField(null=True, blank=True)
   
   def __str__(self) -> str:
     return self.content
